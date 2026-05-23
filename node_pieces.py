808f5ff838b4de1749875e2b391bd8114d13b720
Tyler Osso <tosso2@nic.edu>
2026-05-23 16:41:12 -0700

    nodes created

---
+++ node_pieces.py
@@ -0,0 +1,30 @@
+COSTS = {'R': 1, 'F': 3, 'O': 5, 'H': 8, 'M': 15, 'W': None, 'S': 3, 'E': 3}
+class Node:
+
+
+    def __init__(self, position, parent=None, cost=0):
+            self.position = position
+            self.parent = parent
+            self.cost = cost
+    
+    def __lt__(self, other):
+            return self.cost < other.cost
+    
+def retrace_path(node):
+    path = []
+    while node:
+        path.append(node.position)
+        node = node.parent
+    return list(reversed(path))
+
+def get_neighbors(grid, position):
+    directions = [(-1,0), (1,0), (0,-1),(0,1)]
+    neighbors = []
+    rows, cols = len(grid), len(grid[0])
+    for dr, dc in directions:
+        r, c = position[0] + dr, position[1] + dc
+        if 0 <= r < rows and 0 <= c < cols:
+            if COSTS[grid[r][c]] is not None:  # not water
+                neighbors.append((r, c))
+    return neighbors
+
