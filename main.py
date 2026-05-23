c265375922f9868a5338fda8ccfa843b1c8ec4bf
Tyler Osso <tosso2@nic.edu>
2026-05-23 16:27:40 -0700

    remove unneeded files

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ grid, start, end = load_map("maps/map1_stupid.txt")
 display(grid, closed=set(), open_list=set(), path=[], title="test")
 # debugging print
 print(start, end)
+
+grid, start, end = load_map("maps/map2_simple.txt")
+
+display(grid, closed=set(), open_list=set(), path=[], title="test")
+# debugging print
+print(start, end)
+
+grid, start, end = load_map("maps/map3_complex.txt")
+
+display(grid, closed=set(), open_list=set(), path=[], title="test")
+# debugging print
+print(start, end)
