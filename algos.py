1aaf404b539a1aa973ce7fd11f957e095608869e
Tyler Osso <tosso2@nic.edu>
2026-05-24 11:01:26 -0700

    All coding done

--- algos.py
+++ algos.py
@@ -1,3 +1,4 @@
+import heapq
 from collections import deque
 from node_pieces import *
 
@@ -35,5 +36,131 @@ def bfs(grid, start, end):
     return None, closed, set()  # no path found
 
 """Uniform Cost Search
-    
+        Will pick the cheapest route and only keep the cheapest
+        Will weigh all options and choose the cheapest
+        Continue Path until a solution is found
+        Will order the Solutions keeping only the cheapest
 """
+def ucs(grid, start, end):
+    start_node = Node(start, cost=0)
+    open_queue = []
+    heapq.heappush(open_queue, start_node)
+    
+    closed = set()
+    
+    while open_queue:
+        current = heapq.heappop(open_queue)
+        
+        if current.position in closed:
+            continue
+        closed.add(current.position)
+        
+        if current.position == end:
+            path = retrace_path(current)
+            open_list = {node.position for node in open_queue}
+            return path, closed, open_list, current.cost
+        
+        for neighbor_pos in get_neighbors(grid, current.position):
+            if neighbor_pos not in closed:
+                new_cost = current.cost + COSTS[grid[neighbor_pos[0]][neighbor_pos[1]]]
+                neighbor_node = Node(neighbor_pos, parent=current, cost=new_cost)
+                heapq.heappush(open_queue, neighbor_node)
+    
+    return None, closed, set()
+
+"""Manhattan heuristic
+    uses the shortest grid distance from start to finish to find 
+    a good "Price is right" guess"""
+def manhattan(pos, end):
+    return abs(pos[0] - end[0]) + abs(pos[1] - end[1])
+
+def euclidean(pos, end):
+    return ((pos[0] - end[0])**2 + (pos[1] - end[1])**2) ** 0.5
+
+"""Greedy Best-First Search
+    Uses Heuristic (guess) to find the shortest path w/o worrying about cost or alternates
+    
+    """
+def greedy(grid, start, end):
+    start_node = Node(start, cost=0)
+    open_queue = []
+    heapq.heappush(open_queue, (manhattan(start, end), start_node))
+    
+    closed = set()
+    
+    while open_queue:
+        _, current = heapq.heappop(open_queue)
+        
+        if current.position in closed:
+            continue
+        closed.add(current.position)
+        
+        if current.position == end:      
+            path = retrace_path(current)
+            open_list = {node.position for _, node in open_queue}
+            return path, closed, open_list, current.cost
+                
+        for neighbor_pos in get_neighbors(grid, current.position):
+            if neighbor_pos not in closed:
+                new_cost = current.cost + COSTS[grid[neighbor_pos[0]][neighbor_pos[1]]]
+                neighbor_node = Node(neighbor_pos, parent=current, cost=new_cost)
+                h = manhattan(neighbor_pos, end)
+                heapq.heappush(open_queue, (h, neighbor_node))
+    
+    return None, closed, set()
+
+def astar_manhattan(grid, start, end):
+    start_node = Node(start, cost=0)
+    open_queue = []
+    heapq.heappush(open_queue, (manhattan(start, end), start_node))
+    
+    closed = set()
+    
+    while open_queue:
+        _, current = heapq.heappop(open_queue)
+        
+        if current.position in closed:
+            continue
+        closed.add(current.position)
+        
+        if current.position == end:
+            path = retrace_path(current)
+            open_list = {node.position for _, node in open_queue}
+            return path, closed, open_list, current.cost
+        
+        for neighbor_pos in get_neighbors(grid, current.position):
+            if neighbor_pos not in closed:
+                new_cost = current.cost + COSTS[grid[neighbor_pos[0]][neighbor_pos[1]]]
+                neighbor_node = Node(neighbor_pos, parent=current, cost=new_cost)
+                f = new_cost + manhattan(neighbor_pos, end)
+                heapq.heappush(open_queue, (f, neighbor_node))
+    
+    return None, closed, set(), 0
+
+def astar_euclidean(grid, start, end):
+    start_node = Node(start, cost=0)
+    open_queue = []
+    heapq.heappush(open_queue, (euclidean(start, end), start_node))
+    
+    closed = set()
+    
+    while open_queue:
+        _, current = heapq.heappop(open_queue)
+        
+        if current.position in closed:
+            continue
+        closed.add(current.position)
+        
+        if current.position == end:
+            path = retrace_path(current)
+            open_list = {node.position for _, node in open_queue}
+            return path, closed, open_list, current.cost
+        
+        for neighbor_pos in get_neighbors(grid, current.position):
+            if neighbor_pos not in closed:
+                new_cost = current.cost + COSTS[grid[neighbor_pos[0]][neighbor_pos[1]]]
+                neighbor_node = Node(neighbor_pos, parent=current, cost=new_cost)
+                f = new_cost + euclidean(neighbor_pos, end)
+                heapq.heappush(open_queue, (f, neighbor_node))
+    
+    return None, closed, set(), 0
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from algos import bfs
+from algos import *
 from read_file_to_map import load_map
 from display import display
 import os
@@ -6,17 +6,84 @@ import os
 def run_BFS(PATHNAME):
     grid, start, end = load_map(PATHNAME)
     path, closed, open_list = bfs(grid, start, end)
-    print(f"Path length: {len(path)}")
-    print(f"States explored: {len(closed)}")
+    print(f"\n{PATHNAME} Path length: {len(path)}")
+    print(f"{PATHNAME} States explored: {len(closed)}")
     display(grid, closed, open_list, path, title=PATHNAME)
     filename = os.path.basename(PATHNAME)        # "map1_stupid.txt"
     name = os.path.splitext(filename)[0]         # "map1_stupid"
     NEW_PATHNAME = f"Solutions/BFS/{name}.png"
     os.rename(PATHNAME+".png", NEW_PATHNAME)
 
+def run_UCS(PATHNAME):
+    grid, start, end = load_map(PATHNAME)
+    path, closed, open_list, cost = ucs(grid, start, end)
+    print(f"\n{PATHNAME} Path length: {len(path)}")
+    print(f"{PATHNAME} States explored: {len(closed)}")
+    print(f"{PATHNAME} Cost: {cost}")
+    display(grid, closed, open_list, path, title=PATHNAME)
+    filename = os.path.basename(PATHNAME)        # "map1_stupid.txt"
+    name = os.path.splitext(filename)[0]         # "map1_stupid"
+    NEW_PATHNAME = f"Solutions/UCS/{name}.png"
+    os.rename(PATHNAME+".png", NEW_PATHNAME)
+
+def run_GREEDY(PATHNAME):
+    grid, start, end = load_map(PATHNAME)
+    path, closed, open_list, cost = greedy(grid, start, end)
+    print(f"\n{PATHNAME} Path length: {len(path)}")
+    print(f"{PATHNAME} States explored: {len(closed)}")
+    print(f"{PATHNAME} Cost: {cost}")
+    display(grid, closed, open_list, path, title=PATHNAME)
+    filename = os.path.basename(PATHNAME)        # "map1_stupid.txt"
+    name = os.path.splitext(filename)[0]         # "map1_stupid"
+    NEW_PATHNAME = f"Solutions/GREEDY_BF/{name}.png"
+    os.rename(PATHNAME+".png", NEW_PATHNAME)
+
+def run_ASTAR_MANHATTAN(PATHNAME):
+    grid, start, end = load_map(PATHNAME)
+    path, closed, open_list, cost = astar_manhattan(grid, start, end)
+    print(f"\n{PATHNAME} Path length: {len(path)}")
+    print(f"{PATHNAME} States explored: {len(closed)}")
+    print(f"{PATHNAME} Cost: {cost}")
+    display(grid, closed, open_list, path, title=PATHNAME)
+    filename = os.path.basename(PATHNAME)
+    name = os.path.splitext(filename)[0]
+    NEW_PATHNAME = f"Solutions/ASTAR_MANHATTAN/{name}.png"
+    os.rename(PATHNAME+".png", NEW_PATHNAME)
+
+def run_ASTAR_EUCLIDEAN(PATHNAME):
+    grid, start, end = load_map(PATHNAME)
+    path, closed, open_list, cost = astar_euclidean(grid, start, end)
+    print(f"\n{PATHNAME} Path length: {len(path)}")
+    print(f"{PATHNAME} States explored: {len(closed)}")
+    print(f"{PATHNAME} Cost: {cost}")
+    display(grid, closed, open_list, path, title=PATHNAME)
+    filename = os.path.basename(PATHNAME)
+    name = os.path.splitext(filename)[0]
+    NEW_PATHNAME = f"Solutions/ASTAR_EUCLIDEAN/{name}.png"
+    os.rename(PATHNAME+".png", NEW_PATHNAME)   
 
 if __name__ == "__main__":
+    print("============RUNNING BFS============")
     run_BFS("maps/map1_stupid.txt")
     run_BFS("maps/map2_simple.txt")
     run_BFS("maps/map3_complex.txt")
 
+    print("\n============RUNNING UCS============")
+    run_UCS("maps/map1_stupid.txt")
+    run_UCS("maps/map2_simple.txt")
+    run_UCS("maps/map3_complex.txt")
+
+    print("\n============RUNNING GREEDY============")
+    run_GREEDY("maps/map1_stupid.txt")
+    run_GREEDY("maps/map2_simple.txt")
+    run_GREEDY("maps/map3_complex.txt")
+
+    print("\n============RUNNING A* MANHATTAN============")
+    run_ASTAR_MANHATTAN("maps/map1_stupid.txt")
+    run_ASTAR_MANHATTAN("maps/map2_simple.txt")
+    run_ASTAR_MANHATTAN("maps/map3_complex.txt")
+
+    print("\n============RUNNING A* EUCLIDEAN============")
+    run_ASTAR_EUCLIDEAN("maps/map1_stupid.txt")
+    run_ASTAR_EUCLIDEAN("maps/map2_simple.txt")
+    run_ASTAR_EUCLIDEAN("maps/map3_complex.txt")
